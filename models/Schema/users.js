13745159fd864a4742d1a0d87e0dad9437319c76
Asamanja Kasundi <asamanjakasundi@gmail.com>
2023-11-27 00:36:43 +0530

    added more attributes on schema and changed router name

--- models/Schema/users.js
+++ models/Schema/users.js
@@ -4,6 +4,11 @@ const { Schema } = mongoose;
 const usersSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String },
+  DOB : { type : String},
+  phone_no: {type : Number},
+  email: {type: String},
+  password: {type: String},
+  salt: {type: String}
 });
 
 const UsersModel = mongoose.model("Users", usersSchema);
