51c49fa47c2d2788b7173e72a906cc23cb5cd8ce
Asamanja Kasundi <asamanjakasundi@gmail.com>
2023-12-12 00:10:36 +0530

    validation schema update for phone number

--- API/User/validationSchema.js
+++ API/User/validationSchema.js
@@ -1,4 +1,5 @@
 const yup = require("yup");
+const phoneRegex = /^(?:(?:0|91)?[6789]\d{9})$/
 const createUserSchema = yup.object({
   body: yup.object({
     firstName: yup.string().required(),
@@ -7,6 +8,7 @@ const createUserSchema = yup.object({
       .string()
       .required("Please enter a password")
       .min(8, "Password length should be minimum 8 characters"),
+    phone_no: yup.string().matches(phoneRegex, 'Phone no is not valid').min(10).max(10),
     email: yup
       .string()
       .required("Email is required")
