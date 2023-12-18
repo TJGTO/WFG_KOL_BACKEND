954c01994536ffedfef6f4be97d80a40710de775
Asamanja Kasundi <asamanjakasundi@gmail.com>
2023-12-19 01:12:35 +0530

    .. Add address validation

--- models/Schema/users.js
+++ models/Schema/users.js
@@ -5,8 +5,28 @@ const usersSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String },
   DOB : { type : String},
+  academic: {type : String , enum: ['Working Professional', 'Student']},
   phone_no: {type : String},
   email: {type: String},
+  address: {
+    address_line_1 : {
+      type: String
+    },
+    address_line_2: {
+      type: String
+    },
+    pincode: {
+      type: Number
+    },
+    state: {
+      state_id: {
+        type: Schema.Types.ObjectId
+      },
+      state_name : {
+        type: String
+      }
+    }
+  },
   password: {type: String},
   facebook: {type: String},
   instagram: {type: String},
--- utils/middlewares/requestValidator.js
+++ utils/middlewares/requestValidator.js
@@ -3,6 +3,7 @@ module.exports = (schema) =>
   catchAsync(async (req, res, next) => {
     await schema.validate({
       body: req.body,
+      abortEarly: false 
     });
     return next();
   });
