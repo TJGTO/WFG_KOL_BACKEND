f52316d9426d9b588f0b3c71eae08680ad23b3e0
Asamanja Kasundi <asamanjakasundi@gmail.com>
2023-12-19 01:48:25 +0530

    ..

--- API/User/loginuser.js
+++ API/User/loginuser.js
@@ -4,5 +4,6 @@ const responseHandler = require("../../utils/responseHandler");
 
 module.exports = catchAsync(async (req, res, next) => {
   let payload = await new userService().loginuser(req.body);
+  console.log("Volenath");
   responseHandler(true, payload, res);
 });
