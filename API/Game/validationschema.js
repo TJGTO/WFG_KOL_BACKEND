3fb7e184817a36294f557be968dd1f15e97f346b
Asamanja Kasundi <asamanjakasundi@gmail.com>
2024-01-05 02:01:26 +0530

    build validation schema for addUpdate player

--- API/Game/validationschema.js
+++ API/Game/validationschema.js
@@ -1,13 +1,35 @@
-const yup = require('yup');
+const yup = require("yup");
 
 const createGameSchema = yup.object({
-    body: yup.object({
-        venue: yup.string().required(),
-        date: yup.string().required(),
-        start_time: yup.string().required(),
-        end_time: yup.string().required(),
-        price: yup.string().required(),
-    }),
+  body: yup.object({
+    venue: yup.string().required(),
+    date: yup.string().required(),
+    start_time: yup.string().required(),
+    end_time: yup.string().required(),
+    price: yup.string().required(),
+  }),
 });
 
-module.exports = {createGameSchema};
+const playerSchema = yup.object({
+  name: yup.string().required("Name is required"),
+  player_id: yup.string().required("Player id is required"),
+  age: yup
+    .number()
+    .positive()
+    .max(100)
+    .required("Age should be a positive number"),
+  profilepictureurl: yup
+    .string()
+    .required("Please provide a valid profile picture"),
+});
+const addUpdatePlayerSchema = yup.object({
+  body: yup.object({
+    gameid: yup.string().required(),
+    players: yup
+      .array()
+      .of(playerSchema)
+      .min(1, "At least one player is required"),
+  }),
+});
+
+module.exports = { createGameSchema, addUpdatePlayerSchema };
