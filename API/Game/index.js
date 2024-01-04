fa441b1f30fe07d7052ab9ea1072230fdae10005
Asamanja Kasundi <asamanjakasundi@gmail.com>
2024-01-05 02:01:26 +0530

    build validation schema for addUpdate player

--- API/Game/index.js
+++ API/Game/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const requestValidator = require("../../utils/middlewares/requestValidator");
-const {createGameSchema} = require('./validationschema')
+const {createGameSchema, addUpdatePlayerSchema} = require('./validationschema')
 
 router.post(
     '/create',
@@ -11,7 +11,13 @@ router.post(
 
 router.post(
     '/addUpdate',
+    requestValidator(addUpdatePlayerSchema),
     require('./addUpdatePlayer')
+)
+
+router.delete(
+    '/removePlayer',
+    require('./removeplayer')
 )
 
 module.exports = router;
