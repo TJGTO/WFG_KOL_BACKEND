fbb274ec8617eb4c6c92c7036b75b2772137bf71
Asamanja Kasundi <asamanjakasundi@gmail.com>
2024-01-06 00:55:50 +0530

    added about section

--- API/index.js
+++ API/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.use("/user", require("./User"));
 router.use(require("./States"));
+router.use(require("./AboutUs"));
 router.use("/game", require("./Game"));
 
 module.exports = router;
