92e61b906ac4ae234831c9d6da56480f6622c34d
Tathagata Mondal <tathagata.mandal@zimyo.com>
2024-03-02 01:42:05 +0530

    email testing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 const globalErrorHandler = require("./utils/middlewares/globalErrorHandler");
 const { connectDB } = require("./utils/dbConfig");
 const fileupload = require("express-fileupload");
@@ -22,6 +23,31 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.use(globalErrorHandler);
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "tathagata5121@gmail.com",
+    pass: process.env.mailpassword,
+  },
+});
 
+app.get("/sendEmail", async (req, res) => {
+  try {
+    const info = await transporter.sendMail({
+      from: "tathagata5121@gmail.com", // sender address
+      to: "asamanjakasundi@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+    res.send(info.messageId);
+  } catch (error) {
+    console.log(error);
+    res.send("Failed");
+  }
+});
+
+app.use(globalErrorHandler);
 app.listen(8000, () => console.log("connected"));
