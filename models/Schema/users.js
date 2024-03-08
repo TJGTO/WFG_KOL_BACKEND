660495d4d19071d25dee48e5b0e910beae98a2be
Tathagata Mondal <tathagata.mandal@zimyo.com>
2024-03-08 16:14:54 +0530

    ..timestamp added into users collection

--- models/Schema/users.js
+++ models/Schema/users.js
@@ -1,48 +1,51 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const usersSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String },
-  DOB: { type: String },
-  academic: { type: String, enum: ["Working Professional", "Student"] },
-  phone_no: { type: String },
-  email: { type: String },
-  profilePictureURL: { type: String },
-  roles: {
-    type: [String],
-    default: [],
-    required: true,
-  },
-  address: {
-    address_line_1: {
-      type: String,
+const usersSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String },
+    DOB: { type: String },
+    academic: { type: String, enum: ["Working Professional", "Student"] },
+    phone_no: { type: String },
+    email: { type: String },
+    profilePictureURL: { type: String },
+    roles: {
+      type: [String],
+      default: [],
+      required: true,
     },
-    address_line_2: {
-      type: String,
-    },
-    pincode: {
-      type: String,
-    },
-    city: {
-      type: String,
-    },
-    state: {
-      state_id: {
-        type: Schema.Types.ObjectId,
-      },
-      state_name: {
+    address: {
+      address_line_1: {
         type: String,
       },
+      address_line_2: {
+        type: String,
+      },
+      pincode: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        state_id: {
+          type: Schema.Types.ObjectId,
+        },
+        state_name: {
+          type: String,
+        },
+      },
     },
+    password: { type: String },
+    facebook: { type: String },
+    instagram: { type: String },
+    youtube: { type: String },
+    about: { type: String },
+    salt: { type: String },
   },
-  password: { type: String },
-  facebook: { type: String },
-  instagram: { type: String },
-  youtube: { type: String },
-  about: { type: String },
-  salt: { type: String },
-});
+  { timestamps: true }
+);
 
 const UsersModel = mongoose.model("Users", usersSchema);
 
