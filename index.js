f355a644f94e38d8c84019a63b4e3c09563df863
Tathagata Mondal <tathagata.mandal@zimyo.com>
2024-03-19 01:04:45 +0530

    ..payload limit exceeds

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ connectDB();
 app.use(cors());
 
 app.use(fileupload());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "70mb" }));
+app.use(express.urlencoded({ extended: true, limit: "70mb" }));
 
 app.use(require("./API"));
 
