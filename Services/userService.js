b24b688def3ee64fc4a3bf285652f35ca93a205e
Tathagata Mondal <tathagata.mandal@zimyo.com>
2024-05-22 02:18:32 +0530

    ..update in user details api

--- Services/userService.js
+++ Services/userService.js
@@ -2,12 +2,14 @@ var jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const moment = require("moment");
 const R = require("ramda");
+const formatDate = require("../utils/functions");
 // const CsvParser = require("json2csv").Parser;
 const Exceljs = require("exceljs");
 const { UsersModel } = require("../models/Schema/users");
 const logger = require("../utils/loggerConfig");
 const AWSService = require("./amazonService");
 const GoogleDriveService = require("./gooleDriveService");
+const { ObjectId } = require("mongodb");
 
 module.exports = class Userservice {
   constructor() {
@@ -116,14 +118,53 @@ module.exports = class Userservice {
         cansaveProfilePicture = true;
         userid = data.user.id;
       }
-      const user = await this.userModel.findById({ _id: userid });
-      if (!user) {
+      //const user = await this.userModel.findById({ _id: userid });
+      let user = await this.userModel.aggregate([
+        {
+          $match: {
+            _id: new ObjectId(userid),
+          },
+        },
+        {
+          $lookup: {
+            from: "membershiprecords",
+            localField: "_id",
+            foreignField: "userId",
+            as: "membershipDetails",
+          },
+        },
+        {
+          $project: {
+            password: 0,
+            salt: 0,
+            "membershipDetails._id": 0,
+            "membershipDetails.__v": 0,
+            "membershipDetails.createdAt": 0,
+            "membershipDetails.updatedAt": 0,
+            "membershipDetails.profilePictureURL": 0,
+            __v: 0,
+          },
+        },
+      ]);
+      if (!user || user.length == 0) {
         throw Error("User profile not found");
       }
-      const userObj = JSON.parse(JSON.stringify(user));
-      delete userObj.password;
-      delete userObj.salt;
+      const userObj = JSON.parse(JSON.stringify(user[0]));
       userObj.cansaveProfilePicture = cansaveProfilePicture;
+      let currentDate = new Date();
+      if (
+        userObj.membershipDetails.length > 0 &&
+        currentDate.getTime() <
+          new Date(userObj.membershipDetails[0].validto).getTime()
+      ) {
+        userObj.activemembership = true;
+      } else {
+        userObj.activemembership = false;
+      }
+      userObj.membershipDetails.forEach((x) => {
+        x.validto = formatDate(x.validto, "DD MMM YYYY");
+        x.validfrom = formatDate(x.validfrom, "DD MMM YYYY");
+      });
       return userObj;
     } catch (error) {
       throw new Error("Failed to fetch user profile details");
